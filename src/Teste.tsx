1b6ef2bd86f4f74d5f3ae9d33bbb14257c364bd6
Evandro Santos Afonso <evandrosantosafonso@live.com>
2023-11-09 09:57:16 -0300

    configurando o ambiente de desenvolvimento

---
+++ src/Teste.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Botao = styled.button`
+  background-color: red;
+`
+
+function Teste() {
+  return <Botao>Clicque Aqui</Botao>
+}
+
+export default Teste
