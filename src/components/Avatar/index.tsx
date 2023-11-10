aaae685582773e3b8d2f50b79bd53c508f299828
Evandro Santos Afonso <evandrosantosafonso@live.com>
2023-11-10 11:43:47 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './style'
+
+const Avatar = () => <FotoPerfil src="https://github.com/ordnav3.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/ordnav3.png" alt="" />
-    <Titulo fontSize={20}>Evandro Santos Afonso</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Evandro Santos</Titulo>
+      <Paragrafo fontSize={16} tipo="secundario">
+        ordnav3
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Estudande de Desenvolvimento Front End
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
